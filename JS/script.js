1f2ffa38b8330aa0bcb5d665eb06e63bdce540af
“Tomas <tomas51342679@gmail.com>
2024-03-06 08:12:57 -0300

    Se modifico el codigo y el nombre de un archivo

--- JS/script.js
+++ JS/script.js
@@ -1,41 +1,3 @@
-const prendas = ["Camisa","Remera","Pantalon","Zapatillas"]
-const colores = ["Amarillo","Azul"]
-function vender(){
-    let prendaElegida = prompt(`Que prenda desea comprar?
-1:Camisa 2:Remera 3:Pantalon 4:Zapatillas`)
-    if (parseInt(prendaElegida) == 1  || parseInt(prendaElegida) == 2 || parseInt(prendaElegida) == 3 || parseInt(prendaElegida) == 4){
-        if (prendaElegida == 1){
-            prendaElegida = prendas[0]
-        }else if(prendaElegida == 2){
-            prendaElegida = prendas[1]
-        }else if(prendaElegida == 3){
-            prendaElegida = prendas[2]
-        }else if(prendaElegida == 4){
-            prendaElegida = prendas[3]
-        }
-        let colorPrenda = prompt(`Quiere en color Amarillo o Azul?`)
-        if (colorPrenda == "Amarillo"  || colorPrenda == "amarillo" || colorPrenda == "Azul" || colorPrenda == "azul"){
-            alert(`Su compra de ${prendaElegida} en color ${colorPrenda} a sido exitosa `)
-            preguntar()
-            }else{
-                alert(`No disponemos de ese color`)
-                preguntar()
-            }
-    }else{
-        alert("No tenemos esa prenda")
-        preguntar()
-    }
-}
-function preguntar(){
-    let SegundaCompra = prompt(`Desea comprar otra vez?
-    1:SI 2:NO`)
-    if (SegundaCompra == 1){
-        vender()
-    }else{
-        alert("Muchas gracias por su compra")
-    } 
-}
-vender();
 
 
 
@@ -71,6 +33,126 @@ vender();
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const prendas = ["Camisa","Remera","Pantalon","Zapatillas"]
+// const colores = ["Amarillo","Azul"]
+// function vender(){
+//     let prendaElegida = prompt(`Que prenda desea comprar?
+// 1:Camisa 2:Remera 3:Pantalon 4:Zapatillas`)
+//     if (parseInt(prendaElegida) == 1  || parseInt(prendaElegida) == 2 || parseInt(prendaElegida) == 3 || parseInt(prendaElegida) == 4){
+//         if (prendaElegida == 1){
+//             prendaElegida = prendas[0]
+//         }else if(prendaElegida == 2){
+//             prendaElegida = prendas[1]
+//         }else if(prendaElegida == 3){
+//             prendaElegida = prendas[2]
+//         }else if(prendaElegida == 4){
+//             prendaElegida = prendas[3]
+//         }
+//         let colorPrenda = prompt(`Quiere en color Amarillo o Azul?`)
+//         if (colorPrenda == "Amarillo"  || colorPrenda == "amarillo" || colorPrenda == "Azul" || colorPrenda == "azul"){
+//             alert(`Su compra de ${prendaElegida} en color ${colorPrenda} a sido exitosa `)
+//             preguntar()
+//             }else{
+//                 alert(`No disponemos de ese color`)
+//                 preguntar()
+//             }
+//     }else{
+//         alert("No tenemos esa prenda")
+//         preguntar()
+//     }
+// }
+// function preguntar(){
+//     let SegundaCompra = prompt(`Desea comprar otra vez?
+//     1:SI 2:NO`)
+//     if (SegundaCompra == 1){
+//         vender()
+//     }else{
+//         alert("Muchas gracias por su compra")
+//     } 
+// }
+// vender();
 
 // let Mica = prompt("Del 1 al 10?") 
 // if((Mica==10)){
